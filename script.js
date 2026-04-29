52c44acc452a5a7b4b4e27fad096928573dda23d
PrixBin <prix.bin14@gmail.com>
2026-04-29 16:39:58 -0300

    Estilização JS

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
+function CalcularAmor() {
+    let nota = document.getElementById("nota").value;
+    console.log("Nota inserida: ", nota);
 
-function CalcularAmor()
-    {
-        let nota = document.getElementById("nota").value;
-        console.log("Nota inserida: ",nota);
-
-        if (nota >=10) {
-            alert("É muito amor!");
-        } else if (nota >= 7) {
-            alert("É amor!");
-        } else {
-            alert("Precisamos melhorar!");
-        }
-        
+    if (nota >= 10) {
+        Swal.fire({
+            title: 'É muito amor!',
+            text: 'Vocês foram feitos um para o outro!',
+            imageUrl: 'vetor.png', // Aqui entra sua imagem
+            imageWidth: 150,
+            imageHeight: 'auto',
+            imageAlt: 'Ícone de Amor',
+            background: 'rgb(252, 227, 231)',
+            confirmButtonColor: '#5f1138'
+        });
+    } else if (nota >= 7) {
+        Swal.fire({
+            title: 'É amor!',
+            text: 'O sentimento é real!',
+            imageUrl: 'vetor.png',
+            imageWidth: 100,
+            background: 'rgb(252, 227, 231)',
+            confirmButtonColor: '#5f1138'
+        });
+    } else {
+        Swal.fire({
+            title: 'Hum...',
+            text: 'Precisamos melhorar!',
+            icon: 'info',
+            background: 'rgb(252, 227, 231)',
+            confirmButtonColor: '#5f1138'
+        });
     }
+}
